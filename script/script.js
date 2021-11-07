3a7a579a49c2d54f72728b24ccc5604c33506211
webdevdelfa <79681166+webdevdelfa@users.noreply.github.com>
2021-11-07 23:41:48 +0500

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,12 @@
+// external js: isotope.pkgd.js
+
+$('.grid').isotope({
+    itemSelector: '.grid-item',
+    percentPosition: true,
+    masonry: {
+        columnWidth: 50,
+        gutter: 50,
+        // isFitWidth: true 
+    }
+  });
+  
